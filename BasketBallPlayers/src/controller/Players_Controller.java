a960087ee92655c5dca72e207f2b65fb1bd5f3c3
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2021-03-07 22:13:26 -0800

    Aaand .... It Runs!

--- BasketBallPlayers/src/controller/Players_Controller.java
+++ BasketBallPlayers/src/controller/Players_Controller.java
@@ -1,4 +1,37 @@
 package controller;
 
-public class Players_Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Players_Controller implements Initializable {
+    public TableView allTable;
+    public TableColumn allJerseyCol;
+    public TableColumn allNameCol;
+    public TableColumn allPointsCol;
+    public TableColumn allTeamColumn;
+    public TableView fewTable;
+    public TableColumn fewJerseyCol;
+    public TableColumn fewNameCol;
+    public TableColumn fewPointsCol;
+    public TableColumn fewTeamColumn;
+    public Button addB;
+    public Button removeB;
+
+    @Override public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void onAddB(ActionEvent actionEvent) {
+        System.out.println("On Add Click");
+    }
+
+    public void onRemoveB(ActionEvent actionEvent) {
+        System.out.println("On Remove Click");
+    }
 }
