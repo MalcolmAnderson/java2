fdbfecc50830bbd7b7815e80347431ae668df3bb
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2021-03-08 21:36:34 -0800

    adding players and binding columns

--- BasketBallPlayers/src/controller/Players_Controller.java
+++ BasketBallPlayers/src/controller/Players_Controller.java
@@ -1,10 +1,14 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.BasketballPlayer;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -23,9 +27,31 @@ public class Players_Controller implements Initializable {
     public Button addB;
     public Button removeB;
 
+    private ObservableList<BasketballPlayer> allPlayers = FXCollections.observableArrayList();
+    private ObservableList<BasketballPlayer> fewPlayers = FXCollections.observableArrayList();
+
+
     @Override public void initialize(URL url, ResourceBundle resourceBundle) {
-        int x = 0;
-        x++;
+        allTable.setItems(allPlayers);
+        allJerseyCol.setCellValueFactory(new PropertyValueFactory<>("jersey"));
+        allNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        allPointsCol.setCellValueFactory(new PropertyValueFactory<>("points"));
+        allTeamColumn.setCellValueFactory(new PropertyValueFactory<>("team"));
+        fewTable.setItems(fewPlayers);
+        fewJerseyCol.setCellValueFactory(new PropertyValueFactory<>("jersey"));
+        fewNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        fewPointsCol.setCellValueFactory(new PropertyValueFactory<>("points"));
+        fewTeamColumn.setCellValueFactory(new PropertyValueFactory<>("team"));
+
+        allPlayers.add(new BasketballPlayer(8, "Kobe Bryant", 867, "Lakers"));
+        allPlayers.add(new BasketballPlayer(11, "Yao Ming", 1234, "Rockets"));
+        allPlayers.add(new BasketballPlayer(22, "Clyde Drexler", 1000, "Rockets"));
+        allPlayers.add(new BasketballPlayer(34, "Hakeem Olajuwan", 2, "Rockets"));
+        allPlayers.add(new BasketballPlayer(45, "Rudy Tomjanovich", 450, "Rockets"));
+        allPlayers.add(new BasketballPlayer(13, "Wilt Chamberlin", 1541, "Lakers"));
+        allPlayers.add(new BasketballPlayer(34, "Shaquille O'Neal", 45, "Lakers"));
+        allPlayers.add(new BasketballPlayer(32, "Magic Johnson", 9786, "Lakers"));
+
     }
 
     public void onAddB(ActionEvent actionEvent) {
--- BasketBallPlayers/src/model/BasketballPlayer.java
+++ BasketBallPlayers/src/model/BasketballPlayer.java
@@ -13,35 +13,12 @@ public class BasketballPlayer {
         this.team = team;
     }
 
-    public int getJersey() {
-        return jersey;
-    }
-
-    public void setJersey(int jersey) {
-        this.jersey = jersey;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getPoints() {
-        return points;
-    }
-
-    public void setPoints(int points) {
-        this.points = points;
-    }
-
-    public String getTeam() {
-        return team;
-    }
-
-    public void setTeam(String team) {
-        this.team = team;
-    }
+    public int getJersey() {return jersey;}
+    public void setJersey(int jersey) {this.jersey = jersey;}
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public int getPoints() {return points;}
+    public void setPoints(int points) {this.points = points;}
+    public String getTeam() {return team;}
+    public void setTeam(String team) { this.team = team;}
 }
